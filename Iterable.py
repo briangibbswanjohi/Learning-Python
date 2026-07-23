2f2657bb4f4467a52bda19d16ba43daf55e5996f
Gibbs <bryangibbz2@gmail.com>
2026-07-23 14:57:52 +0300

    Refactor code by commenting out example functions and loops for clarity

--- Iterable.py
+++ Iterable.py
@@ -1,7 +1,4 @@
 # for x in "Gibbu":
 #     print(x)
-number = 100 
-while number > 0:
-    print(number)
-    number = number // 2
+
     
--- Whileloop.py
+++ Whileloop.py
@@ -1,8 +1,8 @@
-# number = 100 
-# while number > 0:
-#     print(number)
-#     number = number // 2
-command = ""
-while command.upper() != "quit":
-    command = input(">")
-    print("ECHO", command)
+# # number = 100 
+# # while number > 0:
+# #     print(number)
+# #     number = number // 2
+# command = ""
+# while command.upper() != "quit":
+#     command = input(">")
+#     print("ECHO", command)
