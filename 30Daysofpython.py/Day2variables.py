c963c1f120590419b92b20e5191767189a389c75
Gibbs <bryangibbz2@gmail.com>
2026-07-23 16:55:40 +0300

    rgb(70, 224, 224) initial implementation of Day2variables.py with variable assignments and calculations

---
+++ 30Daysofpython.py/Day2variables.py
@@ -0,0 +1,30 @@
+first_name = 'Manchester'
+last_name = 'United'
+full_name = first_name + ' ' + last_name
+print(full_name)
+country = 'England'
+city = 'Manchester'
+year = 1878
+age = 2026 - year
+print('Country:', country), print('City:', city),print('Year:', year), print('Age:', age)
+is_winning = True
+if is_winning:
+    print('Manchester United is winning')
+else:
+    print('Manchester United is not winning')
+
+print(type(first_name), type(last_name), type(full_name), type(country), type(city), type(year), type(age), type(is_winning))
+print(len(full_name)), print(len(str(age)))
+number_one = 5
+number_two = 10
+total = number_one + number_two
+difference = number_one - number_two
+product = number_one * number_two
+division = number_one / number_two
+modulus = number_one % number_two
+Power = number_one ** number_two
+print('Total:', total), print('Difference:', difference), print('Product:', product), print('Division:', division), print('Modulus:', modulus), print('Power:', Power)
+radius = 7
+Area_of_circle = 3.14 * radius ** 2
+perimeter_of_circle = 2 * 3.14 * radius
+print('Area of Circle:', Area_of_circle), print('Perimeter of Circle:', perimeter_of_circle)
