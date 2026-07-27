50e967acb1a3723c7feb211a5149f60788434fe8
Gibbs <bryangibbz2@gmail.com>
2026-07-27 22:14:46 +0300

    Add initial implementation of 07.py and Day3Operators.py with example code and comments

---
+++ 27/07.py
@@ -0,0 +1,6 @@
+print("That was a tiresome day, but we made it through!")
+print(type(36250))
+int = float(36250)
+print(type(int))
+
+#we will use this file to recap a few things we have learned in the past 3 days of python then try and close the gap. No days missed.
